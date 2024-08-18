403d22432fa35bb0a398c3fa9b8cd7926da50a4d
gaotianchi <6159984@gmail.com>
2024-08-18 23:23:36 +0800

    UPDATE 一些不稳定的更新

--- userservice/src/main/java/com/gaotianchi/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/gaotianchi/userservice/UserserviceApplication.java
@@ -1,13 +1,24 @@
 package com.gaotianchi.userservice;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@RestController
 public class UserserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
     }
 
+
+    @GetMapping("/")
+    public String index() throws IOException {
+        return "index";
+    }
 }
