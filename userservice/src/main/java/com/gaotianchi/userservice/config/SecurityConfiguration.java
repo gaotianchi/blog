e8e9ab5105fdd331e7176babdf72c8218806e94b
gaotianchi <6159984@gmail.com>
2024-08-18 23:05:50 +0800

    UPDATA 更新 oauth2 认证成功后的处理逻辑

--- userservice/src/main/java/com/gaotianchi/userservice/config/SecurityConfiguration.java
+++ userservice/src/main/java/com/gaotianchi/userservice/config/SecurityConfiguration.java
@@ -1,23 +1,36 @@
 package com.gaotianchi.userservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Autowired
+    public SecurityConfiguration(OAuth2SuccessHandler oAuth2SuccessHandler) {
+        this.oAuth2SuccessHandler = oAuth2SuccessHandler;
     }
+
+    @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf(AbstractHttpConfigurer::disable)
+                    .oauth2Login(oauth2 -> oauth2.successHandler(oAuth2SuccessHandler))
+                    ;
+
+            return http.build();
+        }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
