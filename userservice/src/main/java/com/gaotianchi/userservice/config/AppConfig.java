f1319adfa3dd8af5096424052d2ba06ff5374374
gaotianchi <6159984@gmail.com>
2024-08-18 23:06:46 +0800

    ADD 添加 webClient Bean 配置

--- userservice/src/main/java/com/gaotianchi/userservice/config/AppConfig.java
+++ userservice/src/main/java/com/gaotianchi/userservice/config/AppConfig.java
@@ -3,11 +3,18 @@ package com.gaotianchi.userservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080")
+                .build();
     }
 }
