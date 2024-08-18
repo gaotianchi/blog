fb1790354ed157cb6a23e917f1582aa08cfedb83
gaotianchi <6159984@gmail.com>
2024-08-18 15:22:11 +0800

    ADD UserDetailsService

---
+++ userservice/src/main/java/com/gaotianchi/userservice/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.gaotianchi.userservice.service;
+
+import com.gaotianchi.userservice.persistence.dao.UserRepo;
+import com.gaotianchi.userservice.persistence.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    @Autowired
+    public MyUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("无法找到用户");
+        }
+
+        return new MyUserDetails(user);
+    }
+}
