d5cd828ec1173fba9602bbec3e597efa5d877ccc
gaotianchi <6159984@gmail.com>
2024-08-27 19:52:53 +0800

    UPDATE 删除没有无用的导入语句。

--- authorizationservice/src/main/java/com/gaotianchi/authorizationservice/config/SecurityConfig.java
+++ authorizationservice/src/main/java/com/gaotianchi/authorizationservice/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.gaotianchi.authorizationservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
--- authorizationservice/src/main/java/com/gaotianchi/authorizationservice/service/RegisteredClientService.java
+++ authorizationservice/src/main/java/com/gaotianchi/authorizationservice/service/RegisteredClientService.java
@@ -18,7 +18,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class RegisteredClientService implements RegisteredClientRepository {
