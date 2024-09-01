229bb14a852e820175de0b207f7f3f812d24660a
gaotianchi <6159984@gmail.com>
2024-09-01 16:03:24 +0800

    更新创建用户的控制器的响应数据

--- resourceservice/src/main/java/com/gaotianchi/resourceservice/web/controller/UserController.java
+++ resourceservice/src/main/java/com/gaotianchi/resourceservice/web/controller/UserController.java
@@ -4,6 +4,8 @@ import com.gaotianchi.resourceservice.entity.UserEntity;
 import com.gaotianchi.resourceservice.service.UserService;
 import com.gaotianchi.resourceservice.web.dto.RegistrationDto;
 import com.gaotianchi.resourceservice.web.error.UserAlreadyExistException;
+import com.gaotianchi.resourceservice.web.otd.UserOtd;
+import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +21,10 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<UserEntity> createNewUser(@RequestBody RegistrationDto registrationDto) {
+    public ResponseEntity<UserOtd> createNewUser(@RequestBody RegistrationDto registrationDto) {
         try {
             UserEntity userEntity = userService.createNewUser(registrationDto.getEmail());
-            return new ResponseEntity<>(userEntity, HttpStatus.CREATED);
+            return new ResponseEntity<>(new UserOtd(userEntity), HttpStatus.CREATED);
         } catch (UserAlreadyExistException e) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
