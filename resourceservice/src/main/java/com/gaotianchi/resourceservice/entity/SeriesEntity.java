0d8fb3da504cbc916572c0e988eee8cf352f31bf
gaotianchi <6159984@gmail.com>
2024-09-02 12:38:46 +0800

    给 SeriesEntity 的articleEntities属性添加默认值

--- resourceservice/src/main/java/com/gaotianchi/resourceservice/entity/SeriesEntity.java
+++ resourceservice/src/main/java/com/gaotianchi/resourceservice/entity/SeriesEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
@@ -21,5 +22,5 @@ public class SeriesEntity {
     private ImageEntity cover;
 
     @OneToMany(mappedBy = "seriesEntity")
-    private Collection<ArticleEntity> articleEntities;
+    private Collection<ArticleEntity> articleEntities = new ArrayList<>();
 }
