e98d376a19c4413b212c7fc2506c0c8a7c6b2cd4
gaotianchi <6159984@gmail.com>
2024-09-05 19:26:26 +0800

    添加 handleEntityNotFoundException 用于捕获 EntityNotFoundException 异常

--- resourceservice/src/main/java/com/gaotianchi/resourceservice/config/GlobalExceptionHandler.java
+++ resourceservice/src/main/java/com/gaotianchi/resourceservice/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gaotianchi.resourceservice.config;
 
+import com.gaotianchi.resourceservice.web.error.EntityNotFoundException;
 import com.gaotianchi.resourceservice.web.response.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public <T> APIResponse<T> handleEntityNotFoundException(EntityNotFoundException e) {
+        return APIResponse.fail(404, e.getLocalizedMessage());
+    }
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public <T> APIResponse<T> handleException(Exception e) {
