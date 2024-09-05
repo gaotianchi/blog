928d5eeb0806acd6e8c4fb0153c6e3e48502b1da
gaotianchi <6159984@gmail.com>
2024-09-05 19:51:14 +0800

    添加 handleEntityAlreadyExistException

--- resourceservice/src/main/java/com/gaotianchi/resourceservice/config/GlobalExceptionHandler.java
+++ resourceservice/src/main/java/com/gaotianchi/resourceservice/config/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.gaotianchi.resourceservice.config;
 
+import com.gaotianchi.resourceservice.web.error.EntityAlreadyExistException;
 import com.gaotianchi.resourceservice.web.error.EntityNotFoundException;
 import com.gaotianchi.resourceservice.web.response.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +17,20 @@ public class GlobalExceptionHandler {
     public <T> APIResponse<T> handleEntityNotFoundException(EntityNotFoundException e) {
         return APIResponse.fail(404, e.getLocalizedMessage());
     }
+
+    @ExceptionHandler(EntityAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public <T> APIResponse<T> handleEntityAlreadyExistException(EntityAlreadyExistException e) {
+        return APIResponse.fail(409, e.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public <T> APIResponse<T> handleIoException(IOException e) {
+        System.out.println("unknown exception: " + e.getMessage());
+        return APIResponse.fail(400, e.getLocalizedMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public <T> APIResponse<T> handleException(Exception e) {
