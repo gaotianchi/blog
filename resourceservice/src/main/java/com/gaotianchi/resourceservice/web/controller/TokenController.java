0b5f7f8920374e4c375de0e247e6af23d48b6107
gaotianchi <6159984@gmail.com>
2024-09-05 19:14:59 +0800

    更新获取 token 的控制器

--- resourceservice/src/main/java/com/gaotianchi/resourceservice/web/controller/TokenController.java
+++ resourceservice/src/main/java/com/gaotianchi/resourceservice/web/controller/TokenController.java
@@ -1,18 +1,27 @@
 package com.gaotianchi.resourceservice.web.controller;
 
+import com.gaotianchi.resourceservice.service.TokenService;
+import com.gaotianchi.resourceservice.web.request.NewTokenRequest;
+import com.gaotianchi.resourceservice.web.response.APIResponse;
+import com.gaotianchi.resourceservice.web.response.TokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TokenController {
 
-//    @PostMapping("/token/new")
-//    public ResponseEntity<TokenResponse> newToken(@RequestBody NewTokenRequest newTokenRequest) {
-//        try {
-//            TokenResponse tokenResponse = tokenService.getTokenResponse(newTokenRequest.getEmail(), newTokenRequest.getPassword());
-//            return new ResponseEntity<>(tokenResponse, HttpStatus.OK);
-//        } catch (AuthenticationException ignored) {
-//
-//        }
-//        return null;
-//    }
+    private final TokenService tokenService;
+
+    @Autowired
+    public TokenController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token/new")
+    public APIResponse<TokenResponse> newToken(@RequestBody NewTokenRequest newTokenRequest) {
+        TokenResponse tokenResponse = tokenService.newToken(newTokenRequest.getEmail(), newTokenRequest.getPassword());
+        return APIResponse.success(tokenResponse);
+    }
 }
