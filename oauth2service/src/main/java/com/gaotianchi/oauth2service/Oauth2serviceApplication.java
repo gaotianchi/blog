c512a122c39beffa999063a513d7873d6867f002
gaotianchi <6159984@gmail.com>
2024-09-06 17:27:40 +0800

    初始化授权服务器项目

---
+++ oauth2service/src/main/java/com/gaotianchi/oauth2service/Oauth2serviceApplication.java
@@ -0,0 +1,13 @@
+package com.gaotianchi.oauth2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2serviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2serviceApplication.class, args);
+    }
+
+}
