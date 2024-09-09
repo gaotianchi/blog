68138d26cc52b914588e3bcdd9c346e2a98875e6
gaotianchi <6159984@gmail.com>
2024-09-09 19:35:03 +0800

    更新资源服务器配置

--- resource/src/main/java/com/gaotianchi/resource/config/SecurityConfig.java
+++ resource/src/main/java/com/gaotianchi/resource/config/SecurityConfig.java
@@ -6,11 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -23,22 +22,18 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/resource/article").hasAuthority("profile")
                                 .requestMatchers(HttpMethod.POST, "/users/new").permitAll()
                                 .requestMatchers("/users/**").hasRole("SUBSCRIBER")
                                 .requestMatchers("/blogger/**").hasRole("BLOGGER")
                                 .anyRequest().permitAll()
                 )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults()))
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(customAuthenticationEntryPoint)  // 捕获认证异常
                         .accessDeniedHandler(customAccessDeniedHandler)  // 捕获授权异常
                 );
         return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
