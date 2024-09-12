17fb34d819d3beac23388b7e1b1187d34e03adc2
gaotianchi <6159984@gmail.com>
2024-09-12 13:25:14 +0800

    更新 cors 配置

--- resource/src/main/java/com/gaotianchi/resource/config/SecurityConfig.java
+++ resource/src/main/java/com/gaotianchi/resource/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.gaotianchi.resource.config;
 
-import com.gaotianchi.resource.web.controller.CustomAccessDeniedHandler;
-import com.gaotianchi.resource.web.controller.CustomAuthenticationEntryPoint;
+import com.gaotianchi.resource.web.controller.hook.BeforeRequestHookAccessDeniedHandler;
+import com.gaotianchi.resource.web.controller.hook.BeforeRequestHookAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,17 +9,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, BeforeRequestHookAuthenticationEntryPoint beforeRequestHookAuthenticationEntryPoint, BeforeRequestHookAccessDeniedHandler beforeRequestHookAccessDeniedHandler) throws Exception {
+        http.cors(c -> {
+            c.configurationSource(corsConfigurationSource());
+        });
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers(HttpMethod.POST, "/users/new").permitAll()
@@ -30,10 +34,21 @@ public class SecurityConfig {
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .jwt(Customizer.withDefaults()))
                 .exceptionHandling(e -> e
-                        .authenticationEntryPoint(customAuthenticationEntryPoint)  // 捕获认证异常
-                        .accessDeniedHandler(customAccessDeniedHandler)  // 捕获授权异常
+                        .authenticationEntryPoint(beforeRequestHookAuthenticationEntryPoint)  // 捕获认证异常
+                        .accessDeniedHandler(beforeRequestHookAccessDeniedHandler)  // 捕获授权异常
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
