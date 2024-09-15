a50b955cd8d72fffa16942013d7ad2ee5aa9c0ac
gaotianchi <6159984@gmail.com>
2024-09-15 18:53:44 +0800

    修复下拉菜单与模态框之间的冲突问题

--- admin-client/src/main.ts
+++ admin-client/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import '@/assets/css/main.css';
 
 const app = createApp(App);
