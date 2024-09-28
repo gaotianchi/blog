3e0860c0042360c6ed9d9baaec9aa04b5c0c7222
gaotianchi <6159984@gmail.com>
2024-09-28 12:49:51 +0800

    完成标签控制器

---
+++ resource/src/main/java/com/gaotianchi/resource/web/controller/TagController.java
@@ -0,0 +1,45 @@
+package com.gaotianchi.resource.web.controller;
+
+import com.gaotianchi.resource.web.response.APIResponse;
+import com.gaotianchi.resource.web.response.PageInfo;
+import com.gaotianchi.resource.web.response.info.TagInfo;
+import com.gaotianchi.resource.web.service.tag.TagService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TagController {
+
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping("/tags/new/{name}")
+    public APIResponse<TagInfo> newTag(@PathVariable String name) {
+        return APIResponse.success(tagService.newTag(name));
+    }
+
+    @DeleteMapping("/tags/delete/{id}")
+    public APIResponse<Void> deleteTag(@PathVariable Long id) {
+        tagService.deleteTag(id);
+        return APIResponse.success();
+    }
+
+    @GetMapping("/tags/info/{id}")
+    public APIResponse<TagInfo> getInfo(@PathVariable Long id) {
+        return APIResponse.success(tagService.getInfo(id));
+    }
+
+    @GetMapping("/tags")
+    public APIResponse<PageInfo<TagInfo>> getPageInfo(@RequestParam(value = "page", required = false, defaultValue = "0") int page) {
+        return APIResponse.success(tagService.getPageInfo(page));
+    }
+
+    @GetMapping("/tag-names")
+    public APIResponse<List<String>> getAllNames() {
+        return APIResponse.success(tagService.getAllNames());
+    }
+}
