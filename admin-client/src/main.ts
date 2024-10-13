52e2b5cc606cdddaba9542c9eee903af75d5504f
gaotianchi <6159984@gmail.com>
2024-10-13 20:25:02 +0800

    移除多余的 css 代码

--- admin-client/src/main.ts
+++ admin-client/src/main.ts
@@ -5,6 +5,8 @@ import { VueMasonryPlugin } from 'vue-masonry';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import '@/assets/css/main.css';
 
 const app = createApp(App);
